ca3c2ce235f1646a0e0c16dd878f8c12af934b76
shadhini <jshadhiniaseka@gmail.com>
2020-05-11 18:17:09 +0530

    debug issues in

--- rain/flo2d_OBS.py
+++ rain/flo2d_OBS.py
@@ -246,7 +246,7 @@ def update_rainfall_obs(flo2d_model, method, grid_interpolation, timestep, start
                 if obs1_station_id != str(-1):
                     print(obs1_station_id)
                     obs1_hash_id = stations_dict_for_obs.get(obs1_station_id)
-
+                    print(obs1_hash_id)
                     ts = extract_obs_rain_15_min_ts(connection=curw_obs_connection, start_time=obs_start, id=obs1_hash_id,
                                                     end_time=end_time)
                     print(ts)
